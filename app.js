5754822e02e1dcb7d88bfe30da557c7d50622a28
Jeimy Jara <jeimyjara@hotmail.com>
2023-03-12 22:03:47 -0500

    back

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const { sequelize } = require('./src/config/database');
-const express = require('express');
-const cors = require('cors');
-const routerApi = require('./src/routes/index');
+const { sequelize } = require("./src/config/database");
+const express = require("express");
+const cors = require("cors");
+const routerApi = require("./src/routes/index");
 const app = express();
-const port = 3000;
+const port = process.env.PORT ; 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 routerApi(app);
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
 app.listen(port, () => {
-    sequelize.sync({ force: false }).then(() => {
-        console.log('Database connected');
-    });
-    console.log(`Example app listening at http://localhost:${port}`);
-    });
+  sequelize.sync({ force: false }).then(() => {
+    console.log("Database connected");
+  });
+  console.log(`Example app listening at http://localhost:${port}`);
+});
